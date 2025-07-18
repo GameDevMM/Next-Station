ab2247d317326fcb8a9a85d02990630bda0014a0
MatheusMarcosWebcore <matheusmarcos.profissional@gmail.com>
2025-07-17 22:23:30 -0300

    velocity system

---
+++ Assets/Script/Remake/Seasons/SummerController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SummerController : MonoBehaviour
+{
+    private void Update()
+    {
+        
+    }
+
+    void VelocityController()
+    {
+
+    }
+}
---
+++ Assets/Script/Remake/TrainSpeedController.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System;
+
+public class TrainSpeedController : MonoBehaviour
+{
+    public enum SpeedLevel
+    {
+        Slow,
+        Medium,
+        Fast
+    }
+
+    [Header ("Speed Settings")]
+    public float slowSpeed;
+    public float mediumSpeed;
+    public float fastSpeed;
+
+    [Header("Current State")]
+    [SerializeField] private SpeedLevel currentSpeedLevel = SpeedLevel.Medium;
+    public float currentSpeed { get; private set; }
+
+    [Header("UI")]
+    public TextMeshProUGUI currentSpeedText;
+
+    void Start()
+    {
+        UpdateSpeedUI();
+    }
+
+    public void IncreaseSpeed()
+    {
+        if((int)currentSpeedLevel < Enum.GetValues(typeof(SpeedLevel)).Length - 1)
+        {
+            currentSpeedLevel++;
+            UpdateSpeedUI();
+        }
+    }
+
+    public void DecreaseSpeed()
+    {
+        if ((int)currentSpeedLevel > 0)
+        {
+            currentSpeedLevel--;
+            UpdateSpeedUI();
+        }
+    }
+
+    private void UpdateSpeedUI()
+    {
+        switch (currentSpeedLevel)
+        {
+            case SpeedLevel.Slow:
+                currentSpeed = slowSpeed;
+                currentSpeedText.text = "Lenta";
+                break;
+
+            case SpeedLevel.Medium:
+                currentSpeed = mediumSpeed;
+                currentSpeedText.text = "Médio";
+                break;
+
+            case SpeedLevel.Fast:
+                currentSpeed = fastSpeed;
+                currentSpeedText.text = "Rápida";
+                break;
+        }
+    }
+}
